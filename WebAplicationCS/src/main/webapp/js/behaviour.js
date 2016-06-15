db29ad4d904fba2c098c7f65dc6410f321c3046c
cuent <c.uent@hotmail.com>
2016-06-15 16:23:08 -0500

    added beahaiour to main page

--- WebAplicationCS/src/main/webapp/js/behaviour.js
+++ WebAplicationCS/src/main/webapp/js/behaviour.js
@@ -1,3 +1,43 @@
 $("#loginfb").click(function(){
-  // call oAuth2
+  FB.getLoginStatus(function(responseStat){
+    if(responseStat.status !== 'connected'){
+      FB.login(function(responselogin){
+        if (responselogin.authResponse) {
+            getUser();
+            window.location.href = "mood.html";
+        }else {
+              $("#status").text('Debe autorizar la applicación mediante Facebook');
+        }
+      });
+    } else {
+      getUser();
+      window.location.href = "mood.html";
+    }
+  });
 });
+
+
+var fieldsrequired = 'id, name, first_name, last_name, age_range, link, gender, locale, picture, timezone, updated_time, verified';
+
+function getUser() {
+  FB.api('/me', 'get', {fields: fieldsrequired}, function(response) {
+    console.log(response);
+  });
+}
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '128488277574036',
+    xfbml      : true,
+    version    : 'v2.6'
+  });
+};
+
+// Load the SDK asynchronously
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
